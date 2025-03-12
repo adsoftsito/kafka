3fc1f7bae2e6e855fb4b1fff1c02c2c04d074209
adsoftsito <adsoft@live.com.mx>
2025-03-11 21:08:47 -0600

    Create producer-kafka.py

---
+++ producer-kafka.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    print('I am an errback', exc_info=excp)
+
+producer.send('people', {"name" : "some_message_bytes"} ).add_callback(on_send_success).add_errback(on_send_error)
+producer.close()
